83720b05b49436d08f32d93f6b2d854e734233d0
Alisson1207 <alisson.viteri01@epn.edu.ec>
2024-07-28 11:30:31 -0500

    Autenticación de login1

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,3 @@
-import com.mongodb.client.MongoClients;
-import com.mongodb.client.MongoClient;
-import com.mongodb.client.MongoCollection;
-import com.mongodb.client.MongoDatabase;
-import org.bson.Document;
-import com.mongodb.client.FindIterable;
-import com.mongodb.client.result.UpdateResult;
-import com.mongodb.client.result.DeleteResult;
-
 import javax.swing.*;
 
 public class Main {
